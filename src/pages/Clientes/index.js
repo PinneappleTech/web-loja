f8c0bdf1fe9772d21ab30bee53d70af0b9db928d
Felipe Leal <felipewanderson2020@gmail.com>
2020-07-19 04:13:18 -0300

    (style) conteudo da pagina clientes

--- src/pages/Clientes/index.js
+++ src/pages/Clientes/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import { MdPersonAdd } from 'react-icons/md';
+import { FaSearch } from 'react-icons/fa';
+
 import NavabarLeft from '../../components/NavbarLeft';
 import Header from '../../components/Header';
 
-import { Container, Content } from './styles';
+import { Container, Content, TableContainer } from './styles';
 
 function Clientes() {
   return (
@@ -18,6 +20,48 @@ function Clientes() {
               <MdPersonAdd size={20} color="#515151" />
               Adicionar Clientes
             </button>
+            <div className="box-search">
+              <header>
+                <div>
+                  <MdPersonAdd size={15} color="#515151" />
+                </div>
+                <span>Clientes</span>
+              </header>
+              <div className="search">
+                <div>
+                  <FaSearch size={15} color="#515151" />
+                  <input type="text" placeholder="Buscar clientes" />
+                </div>
+              </div>
+            </div>
+            <TableContainer striped bordered hover responsive>
+              <thead>
+                <tr>
+                  <th>Cod.</th>
+                  <th>Nome</th>
+                  <th>CPF/CPNJ</th>
+                  <th>Telefone</th>
+                  <th>E-mail</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                  <td className="actions">
+                    <div>
+                      <button type="button" />
+                      <button type="button" />
+                      <button type="button" />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </TableContainer>
           </main>
         </Content>
       </Container>
