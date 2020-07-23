ffc1fa7b1b23bc7041e3d9b1a5d2eb75422655a8
Felipe Leal <felipewanderson2020@gmail.com>
2020-07-23 02:04:33 -0300

    (api):conexao da api com o front-end

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { FaUser } from 'react-icons/fa';
 import { MdLock } from 'react-icons/md';
 import { FiLogIn } from 'react-icons/fi';
 import Logo from '../../assets/Logo.svg';
+import api from '../../services/api';
 
 import { Container, Input, Button } from './styles';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const history = useHistory();
-  const handleNavigateToDashboard = () => {
-    history.push('/dashboard');
+
+  const handleNavigateToDashboard = async () => {
+    const data = {
+      username,
+      password,
+    };
+    try {
+      const response = await api.post('/auth/login/', data);
+      const { token, user } = response.data;
+      localStorage.setItem('@anastore/token', token);
+      localStorage.setItem('@anastore/user', user);
+      history.push('/dashboard');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  function handleUsername(event) {
+    const user = event.target.value;
+    setUsername(user);
+  }
+
+  function handlePassword(event) {
+    const pass = event.target.value;
+    setPassword(pass);
   }
 
   return (
@@ -21,7 +47,12 @@ const Login = () => {
 
       <Input>
         <FaUser color="#ECBA2B" size={20} />
-        <input type="text" placeholder="Usuário" />
+        <input
+          type="text"
+          placeholder="Usuário"
+          name="username"
+          onChange={handleUsername}
+        />
       </Input>
 
       <Input>
@@ -29,13 +60,15 @@ const Login = () => {
         <input
           type="password"
           placeholder="Senha"
+          name="password"
+          onChange={handlePassword}
         />
       </Input>
 
       <Button onClick={() => handleNavigateToDashboard()}>
         <FiLogIn color="#FFF" size={30} />
         ENTRAR
-    </Button>
+      </Button>
     </Container>
   );
 };
